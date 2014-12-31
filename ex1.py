d1e79ae6914fbee1d8fc710be41284a467229639
JamesFu008 <jamesfu008@gmail.com>
2014-12-31 17:24:17 +0800

    first time

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# linux. target for python3
+print "Hello World"
+print "Hello again"
+print "I like type this"
+print "this is fun"
+print 'yay ! printing'
+print "I'd rather like this 'not' "
+print "I said no"
+
